09f6643db0daa90bdcf517a03f5d6c17f244129a
Bob Plotts <bob.plotts@dremio.com>
2023-11-03 11:01:46 -0400

    Added parameters for separator, and token to check.

--- README.md
+++ README.md
@@ -1,31 +1,47 @@
 ### DeleteCases
 
 
-My Downloads directory is a mess.  There are files there and I have no idea which case they belong to.  I have screen grabs, heap dumps, and plenty of log files with no metadata to keep them separated.  
-
-So, I set up a "policy" where I name the directories like this: 
+My Downloads directory was a mess.  There were files in there and I have no idea to which case they belong.  I have gc.logs, heap dumps, and plenty of log files with no metadata to keep them separated. I set up a process where I name the directories like this: 
 ```
 customerName_ticketNumber_mo_da
 ```
 So an example directory name might look like: 
 ```
-IBM_11025_01_09
+FredCo_11025_01_09
 ```
+In this case, we want to use _ for the separator, and 2 as the token to check.  Yes, we count from 1.  
 
-This program looks for sub-directories of the current directory that match this naming convention,
-that is - 4 parts in the name with the parts separated by "_" 
+The program takes two arguments from the command line: 
+```shell
+-s --separator - specify the separator character in the directory name - the default is _
+-t -token - specify the token (ZENDESK ticket number) to check, (count from 1)
+````
 
-It then checks if second token (the ZD ticket number) is CLOSED or SOLVED, and if so it prints an rm command you can 
-cut-n-paste into a shell window to remove the directory.
+In another example, the directory names are just the case numbers - e.g. 12345  Here, the separator does not matter and we want -t (or --token) 1. 
 
-### building. 
+This program lists all the sub-directories of the current directory then, based on separator and token number it splits tha name, then checks Zendesk to see if the case with that number is SOLVED or CLOSED. 
+If so, it prints an `rm` command that you can copy-n-paste into a shell window to remove the directory.
+
+### Building
+```shell
+git clone https://github.com/rcprcp/DeleteCases
+```
 After checking out the repo, go into the root directory for this program and type: 
-```mvn clean package```
-That should build it.   :) 
+```shell
 
-### run it. 
-Maven will build a runnable jar in the `target` directory.
-cd into the directory in which you're storing your cases, directory, then: 
+mvn clean package
+```
+That's it. 
+
+### Running 
+In order to access Zendesk, you will need to configure 3 environment variables. 
+```shell
+export ZENDESK_EMAIL="https://domain.zendesk.com"
+export ZENDESK_EMAIL="bob@domain.com"
+export ZENDESK_TOKEN="c0928blahblahblahblahg1h14g5"
+```
+Maven will build a runnable jar in the `target` subdirectory.
+`cd` into the directory in which you're storing your cases, then: 
 ``` shell
-java -jar ~/DeleteCases/target/deletecases-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar ~/DeleteCases/target/deletecases-1.0-SNAPSHOT-jar-with-dependencies.jar [-s] [-t]
 ```
--- src/main/java/com/cottagecoders/deletecases/DeleteCases.java
+++ src/main/java/com/cottagecoders/deletecases/DeleteCases.java
@@ -1,15 +1,23 @@
 package com.cottagecoders.deletecases;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 import org.apache.commons.lang3.StringUtils;
+import org.zendesk.client.v2.Zendesk;
+import org.zendesk.client.v2.model.Status;
+import org.zendesk.client.v2.model.Ticket;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 public class DeleteCases {
+
+  @Parameter(names = {"-s", "--separator"}, description = "separator for fields in directory name (default is _)")
+  public String separator = "_";
+
+  @Parameter(names = {"-t", "--token"}, description = "token number to check (2 for IBM_12345_10_31)")
+  public int tokenNumber = 2;
+
 
   private DeleteCases() throws IOException {
   }
@@ -17,31 +25,34 @@ public class DeleteCases {
   public static void main(String... args) throws IOException {
 
     if (StringUtils.isEmpty(System.getenv("ZENDESK_URL"))) {
-      System.out.println("Please set the ZENDESK_URL environment variable: `export ZENDESK_EMAIL=\"https://domain.zendesk.com\"`");
+      System.out.println(
+              "Please set the ZENDESK_URL environment variable: `export ZENDESK_EMAIL=\"https://domain.zendesk.com\"`");
       System.exit(5);
     }
 
     if (StringUtils.isEmpty(System.getenv("ZENDESK_EMAIL"))) {
-      System.out.println("Please set the ZENDESK_EMAIL environment variable: `export ZENDESK_EMAIL=\"bob@domain.com\"`");
+      System.out.println("Please set the ZENDESK_EMAIL environment variable: `export ZENDESK_EMAIL=\"bob@domain" +
+                                 ".com\"`");
       System.exit(5);
     }
 
     if (StringUtils.isEmpty(System.getenv("ZENDESK_TOKEN"))) {
-      System.out.println("Please set the ZENDESK_TOKEN environment variable: `export ZENDESK_TOKEN=\"c0928blahblahblahblahg1h14g5\"`");
+      System.out.println("Please set the ZENDESK_TOKEN environment variable: `export " + "ZENDESK_TOKEN" +
+                                 "=\"c0928blahblahblahblahg1h14g5\"`");
       System.exit(5);
     }
 
-    // gather Zendesk info:
-    ZendeskCases zd = new ZendeskCases(System.getenv("ZENDESK_URL"), System.getenv("ZENDESK_EMAIL"), System.getenv("ZENDESK_TOKEN"));
-    Set<Long> zdinfo = zd.getCaseStatus();
-    zd.close();
+    try (Zendesk zd =
+                 new Zendesk.Builder(System.getenv("ZENDESK_URL")).setUsername(System.getenv("ZENDESK_EMAIL")).setToken(
+            System.getenv("ZENDESK_TOKEN")).build()) {
 
-    DeleteCases dc = new DeleteCases();
-    dc.process(zdinfo);
+      DeleteCases dc = new DeleteCases();
+      JCommander.newBuilder().addObject(dc).build().parse(args);
+      dc.process(zd);
+    }
   }
 
-  private void process(Set<Long> zdInfo) throws IOException {
-    Set<String> cases = new HashSet<>();
+  private void process(Zendesk zd) throws IOException {
 
     // gather all directories in the  current directory
     File[] files = new File("./").listFiles(File::isDirectory);
@@ -51,29 +62,21 @@ public class DeleteCases {
     }
 
     for (File file : files) {
-      String[] parts = file.getName().split("_");
-      if (parts.length != 4) {
-        continue;
-      }
+      String[] parts = file.getName().split(separator);
 
-      long caseNumber;
       try {
-        caseNumber = Long.parseLong(parts[1]);
+        if (parts.length > tokenNumber - 1) {
+          Ticket t = zd.getTicket(Long.parseLong(parts[tokenNumber - 1]));
+          if (t != null) {
+            if (t.getStatus().equals(Status.CLOSED) || t.getStatus().equals(Status.SOLVED)) {
+              System.out.println("rm -rf " + file.getCanonicalPath());
+            }
+          }
+        }
       } catch (NumberFormatException ex) {
-        System.out.println("NumberFormatException  on '" + file.getName() + " " + ex.getMessage());
+        //System.out.println("NumberFormatException  on '" + file.getName() + " " + ex.getMessage());
         continue;
       }
-
-      // check ZD status here.
-      if (zdInfo.contains(caseNumber)) {
-        // add it to the map.
-        cases.add(file.getCanonicalPath());
-      }
-
     }
-    for (String path : cases) {
-      System.out.println("rm -rf " + path);
-    }
-    System.out.println("size " + cases.size());
   }
 }
